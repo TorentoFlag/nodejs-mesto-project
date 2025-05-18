f572e1fa9caa5cfe7a7623bb4616e088d63d705b
Anton Golubev <AnDGolubev@srt.beeline.ru>
2025-05-18 16:22:14 +0300

    feat: логгеры, авторизация, регистрация

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,29 @@
-import express, { NextFunction, Response } from 'express';
-import { RequestWithUser } from './interfaces/RequestWithUser';
+import express from 'express';
+import 'dotenv/config';
+import cookieParser from 'cookie-parser';
+import { loginValidator, userValidation } from './middleware/validation';
+import auth from './middleware/auth';
+import { createUser, login } from './controllers/users';
 import connectToDb from './config/db';
 import router from './router';
 import ErrorHandler from './middleware/errorHandler';
+import { requestLogger, errorLogger } from './middleware/logger';
 
 const app = express();
 app.use(express.json());
-
-app.use((req: RequestWithUser, res: Response, next: NextFunction) => {
-  req.user = {
-    _id: '681e49864219fff3d6ece25d',
-  };
-
-  next();
-});
+app.use(cookieParser());
 
 connectToDb();
+
+app.use(requestLogger);
+
+app.post('/signin', loginValidator, login);
+app.post('/signup', userValidation, createUser);
+
+app.use(auth);
 app.use('/', router);
 
+app.use(errorLogger);
 app.use(ErrorHandler);
 
 export default app;
