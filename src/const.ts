b620b9bfe1514fb328f89ad70bf2d6277297cf70
Anton Golubev <AnDGolubev@srt.beeline.ru>
2025-05-18 16:22:40 +0300

    feat: новые константы

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,8 @@
 export const BAD_REQUEST_ERROR_STATUS = 400;
+export const UNAUTHORIZED_ERROR_STATUS = 401;
+export const FORBIDDEN_ERROR_STATUS = 403;
 export const NOT_FOUND_ERROR_STATUS = 404;
+export const CONFLICT_ERROR_STATUS = 409;
 export const INTERNAL_SERVER_ERROR = 500;
+export const JWT_SECRET = 'jwt_secret';
+export const URL_REGEXP = /^(https?):\/\/(w{3}\.)?(?!www)(([А-ЯЁа-яёA-Za-z0-9_-]+\.[А-ЯЁа-яёA-Za-z0-9_-]+(\.[А-ЯЁа-яёA-Za-z_-]+){0,2})|(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)))(:\d{2,5})?(\/[A-Za-z0-9/\-_.#:?&~/=]*)?$/;
