b1137d0203e79f4b6e01130b9aaca0c23b6585b1
Anton Golubev <AnDGolubev@srt.beeline.ru>
2025-05-18 16:24:06 +0300

    feat: проверка авторизации

---
+++ src/middleware/auth.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Response } from 'express';
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../const';
+import { RequestWithUser } from '../interfaces/RequestWithUser';
+import UnauthorizedError from '../errors/UnauthorizedError';
+import NotFoundError from '../errors/NotFoundError';
+
+interface IPayload {
+  _id: string;
+}
+
+const auth = (req: RequestWithUser, res: Response, next: NextFunction) => {
+  const token = req.cookies.jwt;
+  if (!token) {
+    throw new UnauthorizedError('Неверный логин или пароль');
+  }
+
+  if (!JWT_SECRET) {
+    throw new NotFoundError('Не найдена переменная "JWT_SECRET"');
+  }
+
+  let payload;
+
+  try {
+    payload = jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    throw new UnauthorizedError('Ошибка авторизации');
+  }
+
+  req.user = payload as IPayload;
+  next();
+};
+
+export default auth;
